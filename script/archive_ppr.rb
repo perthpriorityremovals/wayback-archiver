a9f0822fb7757e72d23c0ce6bcb4046e3261fa22
perthpriorityremovals <perthpriorityremovals@gmail.com>
2026-03-28 19:42:06 +1100

    Update archive_ppr.rb

--- script/archive_ppr.rb
+++ script/archive_ppr.rb
@@ -2,10 +2,18 @@ require "bundler/setup"
 require "wayback_archiver"
 require "logger"
 
-WaybackArchiver.logger = Logger.new($stdout)
+logger = Logger.new($stdout)
+logger.level = Logger::DEBUG
+
+WaybackArchiver.logger = logger
 WaybackArchiver.concurrency = 1
 WaybackArchiver.max_limit = -1
 
 start_url = "https://perthpriorityremovals.com.au"
 
-WaybackArchiver.archive(start_url)
+puts "Starting archive run for: #{start_url}"
+
+result = WaybackArchiver.archive(start_url)
+
+puts "Archive run finished."
+puts "Result: #{result.inspect}"
